e0869d9f8f37895d078de97190b4ff5ca93b6172
Axel Jimenez <ajimenezq1@ucenfotec.ac.cr>
2025-02-17 23:52:46 -0600

    Remove addUser

--- src/main/java/com/project/demo/rest/user/UserRestController.java
+++ src/main/java/com/project/demo/rest/user/UserRestController.java
@@ -47,15 +47,6 @@ public class UserRestController {
                 ordersPage.getContent(), HttpStatus.OK, meta);
     }
 
-    @PostMapping
-    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
-    public ResponseEntity<?> addUser(@RequestBody User user, HttpServletRequest request) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        return new GlobalResponseHandler().handleResponse("User updated successfully",
-                user, HttpStatus.OK, request);
-    }
-
     @PutMapping("/{userId}")
     @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
     public ResponseEntity<?> updateUser(@PathVariable Long userId, @RequestBody User user, HttpServletRequest request) {
